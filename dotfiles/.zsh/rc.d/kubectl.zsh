a72484eb8e350102905a34ba6c3542ccd173ab67
krag3 <24490796+krag3@users.noreply.github.com>
2024-12-14 13:07:42 +0900

    add alias

--- dotfiles/.zsh/rc.d/kubectl.zsh
+++ dotfiles/.zsh/rc.d/kubectl.zsh
@@ -7,3 +7,6 @@ alias ktl-g='kubectl get all -owide'
 alias ktl-ga='kubectl get all -A -owide'
 alias ktl-w='watch kubectl get all -owide'
 alias ktl-wa='watch kubectl get all -A -owide'
+alias ktl-rr='kubectl rollout restart'
+
+alias cilium='cilium -n cilium'
